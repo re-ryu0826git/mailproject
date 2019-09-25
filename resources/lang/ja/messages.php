7f2500942836674d5a7e152e1379100d18f12931
”TanakaRyunosuke” <ryu0826tanaka@gmail.com>
2019-09-25 19:37:01 +0000

    login view japanese

---
+++ resources/lang/ja/messages.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'Login' => 'ログイン',
+    'E-Mail Address' => 'メールアドレス',
+    'Password' => 'パスワード',
+    'Remember Me' => 'ログイン情報を記憶する',
+    'Register' => '登録',
+    'Confirm Password' => '確認用パスワード',
+    'Name' => '名前',
+    'Reset Password' => 'パスワードリセット',
+    'Send Password Reset Link' => 'パスワードリセットリンクの送信',
+    'Forgot Your Password?' => 'パスワードを忘れた場合'
+];
